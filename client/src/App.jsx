6cd2bc7d5d76771d161d63e945972cf644f33c05
Akash Bhosale <theakashbhosale214>
2024-05-25 10:28:41 +0530

    Added flowbite react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,11 @@ import Dashbord from './pages/Dashbord'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import Projects from './pages/Projects'
-
+import Header from './components/Header'
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path='/about' element={<About />} />
@@ -17,8 +18,6 @@ export default function App() {
       <Route path='/sign-in' element={<SignIn />} />
       <Route path='/sign-up' element={<SignUp />} />
       <Route path='/projects' element={<Projects />} />
-
-
     </Routes>
     </BrowserRouter>
   )
